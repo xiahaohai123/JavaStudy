4aaa9073a7aeb5e369217d8e6d414cffff863e15
summer_sea <xiahaohai@qq.com>
2022-11-15 09:44:57 +0800

    二进制操作，增加左移操作

--- src/main/java/digital_encode/ShiftTest.java
+++ src/main/java/digital_encode/ShiftTest.java
@@ -3,12 +3,13 @@ package digital_encode;
 public class ShiftTest {
     public static void main(String[] args) {
         ShiftTest shiftTest = new ShiftTest();
-        shiftTest.testSignedShift();
-        shiftTest.testUnsignedShift();
+        shiftTest.testSignedShiftRight();
+        shiftTest.testUnsignedShiftRight();
+        shiftTest.testShiftLeft();
     }
 
-    private void testSignedShift() {
-        System.out.println("signedShift: ");
+    private void testSignedShiftRight() {
+        System.out.println("signed shift right: ");
         int positiveA = 18;  // 0000 0000 0000 0000 0000 0000 0001 0010
         System.out.println("positiveA: " + positiveA);
         int a = 18 >> 2;  // a: 0000 0000 0000 0000 0000 0000 0000 0100
@@ -20,8 +21,8 @@ public class ShiftTest {
         System.out.println("b: " + b);
     }
 
-    private void testUnsignedShift() {
-        System.out.println("unsignedShift: ");
+    private void testUnsignedShiftRight() {
+        System.out.println("unsigned shift right: ");
         int positiveA = 18;  // 0000 0000 0000 0000 0000 0000 0001 0010
         System.out.println("positiveA: " + positiveA);
         int a = 18 >>> 2; // a: 0000 0000 0000 0000 0000 0000 0000 0100
@@ -30,6 +31,19 @@ public class ShiftTest {
         int positiveB = -18;  // 1111 1111 1111 1111 1111 1111 1110 1110
         System.out.println("positiveB: " + positiveB);
         int b = -18 >>> 2; // b: 0011 1111 1111 1111 1111 1111 1111 1011
+        System.out.println("b: " + b);
+    }
+
+    private void testShiftLeft() {
+        System.out.println("shift left: ");
+        int positiveA = 18;  // 0000 0000 0000 0000 0000 0000 0001 0010
+        System.out.println("positiveA: " + positiveA);
+        int a = 18 << 2; // a: 0000 0000 0000 0000 0000 0000 0100 1000
+        System.out.println("a: " + a);
+
+        int positiveB = -18;  // 1111 1111 1111 1111 1111 1111 1110 1110
+        System.out.println("positiveB: " + positiveB);
+        int b = -18 << 2; // b: 1111 1111 1111 1111 1111 1111 1011 1000
         System.out.println("b: " + b);
     }
 }
